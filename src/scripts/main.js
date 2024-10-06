abc1ae996b7aa9f573c9242daad3a22f2bd705cd
Fer <fer.falcon.dg@gmail.com>
2024-10-06 09:19:24 -0300

    Data loading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,20 @@
 import loadData from './async-fetch-json-data';
 import printJsonObject from './print-json-object';
 
-const dataUrl = 'https://api.github.com/users/ferfalcon';
+const dataUrl = 'https://api.github.com/users/';
 
-loadData(dataUrl)
-	.then ( data => {
-		printJsonObject(data);
-	})
+
+document.querySelector('[data-search-form]').addEventListener('submit', function(event) {
+	event.preventDefault();
+	const userName = document.querySelector('[data-search-input]').value.trim();
+	if (!userName) {
+		showError('Please enter a username');
+		return;
+	}
+
+	loadData(dataUrl + userName).then(data => printJsonObject(data));
+})
+
+function showError(msg) {
+	console.log(msg)
+}
