66652eca3c695164dffc40741c568cda7e558d18
Fer <fer.falcon.dg@gmail.com>
2024-10-06 10:02:08 -0300

    Search styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,6 @@ import printJsonObject from './print-json-object';
 
 const dataUrl = 'https://api.github.com/users/';
 
-
 document.querySelector('[data-search-form]').addEventListener('submit', function(event) {
 	event.preventDefault();
 	const userName = document.querySelector('[data-search-input]').value.trim();
