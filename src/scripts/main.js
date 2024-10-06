67f2a5e4a942081cb1f29d89a43b335da65378e2
Fer <fer.falcon.dg@gmail.com>
2024-10-06 11:10:24 -0300

    Basic layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,13 @@ const dataUrl = 'https://api.github.com/users/';
 
 document.querySelector('[data-search-form]').addEventListener('submit', function(event) {
 	event.preventDefault();
-	const userName = document.querySelector('[data-search-input]').value.trim();
+	let userName = document.querySelector('[data-search-input]').value.trim();
 	if (!userName) {
 		showError('Please enter a username');
 		return;
 	}
 
+	document.querySelector('[data-search-input]').value = "";
 	loadData(dataUrl + userName).then(data => printJsonObject(data));
 })
 
